b995e49a674b964bb8fb0832fdc1f695d5a9fb66
ZGaziz <g.zhaiyk@p-s.kz>
2021-02-08 17:27:39 +0600

    Added products component

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-
+import {useState, useEffect} from 'react';
+import data from "./data.json";
+import Products from "./components/Products"
 function App() {
-  return (
-    <div className="grid-container">
-      <header>
-      	<a href="/">Gaziz React Shopping Cart</a>
-      </header>
-      <main>
-      	Product List
-      </main>
-      <footer>
-      	All right is reserved
-      </footer>
-    </div>
-  );
+	const [products, setProducts] = useState(data.products)
+	const [size, setSize] = useState("")
+	const [sort, setSort] = useState("")
+
+	const getProducts = () => {
+		setProducts(data.products)
+	}
+	useEffect(() => {
+		getProducts();
+	}, []);
+
+	return (
+		<div className="grid-container">
+			<header>
+				<a href="/">Gaziz React Shopping Cart</a>
+			</header>
+			<main>
+				<div className="content">
+					<div className="main">
+						<Products products={products}></Products>		
+					</div>
+					<div className="sidebar">
+						Card Items
+					</div>
+				</div>
+			</main>
+			<footer>
+				All right is reserved
+			</footer>
+			</div>
+			);
 }
 
 export default App;
---
+++ src/components/Products.js
@@ -0,0 +1,30 @@
+import formatCurrency from "../utils"
+function Products(props) {
+	return (
+		<div>
+			<ul className="products">
+			{props.products.length > 0 ? props.products.map((item) => (
+				<li key={item._id}>
+					<div className="product">
+						<a href={`#/${item._id}`}>
+							<img src={item.image} alt={item.title}></img>
+							<p>{item.title}</p>
+						</a>
+						<div className="product-price">
+							<div>
+								{formatCurrency(item.price)}
+							</div>
+							<button className="button primary">
+								Add to cart
+							</button>
+						</div>
+					</div>
+				</li>
+			)) : "No data"}
+			</ul>
+		</div>
+	)
+};
+
+export default Products;
+
