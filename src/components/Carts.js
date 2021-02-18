a207b284383bdec89d6b7cc9b4c66cfa56e2a11c
ZGaziz <g.zhaiyk@p-s.kz>
2021-02-18 12:52:14 +0600

    aaaa

--- src/components/Carts.js
+++ src/components/Carts.js
@@ -27,19 +27,21 @@ function Carts(props) {
 								}
 							</ul>
 						</div>
-						<div className="cart">
-							{
-							props.cartItems.length > 0 ? <div className="total">
-								<div>
-									Total: {" "}{formatCurrency(props.cartItems.reduce((a,c) => a + c.price * c.count, 0))}
-								</div>
-								<button className="button primary" onClick={props.proceedCartItems}>Proceed</button>
-							</div> : ""
-							}
-						</div>
-					</div>
-					</div>
-					)
+						{	props.cartItems.length !== 0 && 
+							(
+								<div className="cart">
+									<div className="total">
+										<div>
+											Total: {" "}{formatCurrency(props.cartItems.reduce((a,c) => a + c.price * c.count, 0))}
+										</div>
+										<button className="button primary" onClick={props.proceedCartItems}>Proceed</button>
+									</div> 
+									</div>
+									)
+								}
+							</div>
+							</div>
+							)
 };
 
 export default Carts;
